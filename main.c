f190b143b6ffc7fd1256ff949d22db816782cf0e
Aaditya Chandrasekhar <aaditya.c@phiar.net>
2020-09-13 18:24:28 -0700

    Support multiple inputs for ELF patching.

--- main.c
+++ main.c
@@ -21,85 +21,97 @@ main(int32_t argc,
      char **argv)
 {
     if (argc < 2){
-        exit(0);
+        return 0;
     }
 
-    int fd = open(argv[1], O_RDWR);
-    struct stat st;
+    for (int32_t fileIndex = 1; fileIndex < argc; ++fileIndex) {
+        int fd = open(argv[fileIndex], O_RDWR);
+        struct stat file_stat;
 
-    if ((fd == -1) ||
-        (fstat(fd, &st) == -1)) {
-        exit(1);
-    }
-
-    uint8_t *mem = mmap(NULL, st.st_size, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
-    Elf64_Ehdr * const elf_header = (Elf64_Ehdr *)mem;
-    Elf64_Phdr * const program_headers = (Elf64_Phdr *)&mem[elf_header->e_phoff];
-
-    for (size_t i = 0; i < elf_header->e_phnum; ++i) {
-        if (program_headers[i].p_type == PT_DYNAMIC) {
-            Elf64_Dyn * const dyn = (Elf64_Dyn *)&mem[program_headers[i].p_offset];
-            size_t const num_dyn = program_headers[i].p_filesz / sizeof(*dyn);
-            size_t stroffset = 0;
-
-            for (size_t i = 0; i < num_dyn; ++i) {
-                if (dyn[i].d_tag == DT_STRTAB) {
-                    Elf64_Shdr * const section_headers = (Elf64_Shdr *)&mem[elf_header->e_shoff];
-
-                    for (size_t j = 0; j < elf_header->e_shnum; ++j) {
-                        if (section_headers[j].sh_addr == dyn[i].d_un.d_ptr) {
-                            stroffset = section_headers[j].sh_offset;
-                            break;
-                        }
-	             }
-
-                     break;
-                }
-            }
-
-            for (size_t i = 0; i < num_dyn; ++i) {
-                size_t const offset = stroffset + dyn[i].d_un.d_val;
-
-                switch (dyn[i].d_tag) {
-                case DT_NEEDED:
-                case DT_SONAME: {
-                    char const *name = (char *)&mem[offset];
-                    char *suffix_ptr = strstr(name, ".so");
-
-                    // No-op version suffix for dependent libraries
-                    if (suffix_ptr && suffix_ptr[3]) {
-                        suffix_ptr[3] = '\0';
-                    }
-
-                    break;
-                }
-
-                case DT_RPATH:
-                case DT_RUNPATH: {
-                    char const *new_rpath = "$ORIGIN";
-                    char *rpath = (char *)&mem[offset];
-
-                    // RUNPATH is set to $ORIGIN
-                    strcpy(rpath, new_rpath);
-
-                    if (dyn[i].d_tag == DT_RPATH) {
-                        // Coerce tag from DT_RPATH to DT_RUNPATH
-                        dyn[i].d_tag = DT_RUNPATH;
-                    }
-
-                    break;
-                }
-
-                default:
-                    break;
-               }
-            }
-
-            break;
+        if ((fd == -1) ||
+            (fstat(fd, &file_stat) == -1)) {
+            return 1;
         }
+
+        uint8_t *mem = mmap(NULL,
+                            file_stat.st_size,
+                            PROT_READ |
+                            PROT_WRITE,
+                            MAP_SHARED,
+                            fd,
+                            0);
+        Elf64_Ehdr * const elf_header = (Elf64_Ehdr *)mem;
+        Elf64_Phdr * const program_headers = (Elf64_Phdr *)&mem[elf_header->e_phoff];
+
+        for (size_t i = 0; i < elf_header->e_phnum; ++i) {
+            if (program_headers[i].p_type == PT_DYNAMIC) {
+                Elf64_Dyn * const dyn = (Elf64_Dyn *)&mem[program_headers[i].p_offset];
+                size_t const num_dyn = program_headers[i].p_filesz / sizeof(*dyn);
+                size_t stroffset = 0;
+
+                for (size_t i = 0; i < num_dyn; ++i) {
+                    if (dyn[i].d_tag == DT_STRTAB) {
+                        Elf64_Shdr * const section_headers = (Elf64_Shdr *)&mem[elf_header->e_shoff];
+
+                        for (size_t j = 0; j < elf_header->e_shnum; ++j) {
+                            if (section_headers[j].sh_addr == dyn[i].d_un.d_ptr) {
+                                stroffset = section_headers[j].sh_offset;
+                                break;
+                            }
+	                 }
+
+                         break;
+                    }
+                }
+
+                for (size_t i = 0; i < num_dyn; ++i) {
+                    size_t const offset = stroffset + dyn[i].d_un.d_val;
+
+                    switch (dyn[i].d_tag) {
+                    case DT_NEEDED:
+                    case DT_SONAME: {
+                        char const *name = (char *)&mem[offset];
+                        char *suffix_ptr = strstr(name, ".so");
+
+                        // No-op version suffix for dependent libraries
+                        if (suffix_ptr && suffix_ptr[3]) {
+                            suffix_ptr[3] = '\0';
+                        }
+
+                        break;
+                    }
+
+                    case DT_RPATH:
+                    case DT_RUNPATH: {
+                        char const *new_rpath = "$ORIGIN";
+                        char *rpath = (char *)&mem[offset];
+
+                        // RUNPATH is set to $ORIGIN
+                        strcpy(rpath, new_rpath);
+
+                        if (dyn[i].d_tag == DT_RPATH) {
+                            // Coerce tag from DT_RPATH to DT_RUNPATH
+                            dyn[i].d_tag = DT_RUNPATH;
+                        }
+
+                        break;
+                    }
+
+                    default:
+                        break;
+                    }
+                }
+
+                break;
+            }
+        }
+
+        msync(mem,
+              file_stat.st_size,
+              MS_SYNC);
+        munmap(mem,
+               file_stat.st_size);
     }
 
-    msync(mem, st.st_size, MS_SYNC);
-    munmap(mem, st.st_size);
-    exit(0);
+    return 0;
 }
